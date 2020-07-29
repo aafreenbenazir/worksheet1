ebe9da048bd5363dcce4513f5471ece4507c7820
aafreenbenazir <68464108+aafreenbenazir@users.noreply.github.com>
2020-07-29 18:33:19 +0530

    Add files via upload

---
+++ random.java
@@ -0,0 +1,12 @@
+/*
+ K.AAFREEN BENAZIR
+ */
+package worksheet.pkg1;
+
+
+public class random {
+    public static void main(String args[])
+    {
+    System.out.println((int)(Math.random()*100));
+    }
+            }
